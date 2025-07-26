b6131ab2247e8bff4fb5c2d2cb4e3a322959258a
Gianluca <gianlucad1993@gmail.com>
2025-07-26 03:40:19 +0000

    updated firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyBr-z24PPfdEp_IFnXAWr5HwEPodz_IgnI",
   authDomain: "myshowtracker-93954.firebaseapp.com",
   projectId: "myshowtracker-93954",
-  storageBucket: "myshowtracker-93954.firebasestorage.app",
+  storageBucket: "myshowtracker-93954.appspot.com",
   messagingSenderId: "508417592783",
   appId: "1:508417592783:web:b189a1a05e48ae9ba316ba",
   measurementId: "G-5Z7QT4W0R3",
